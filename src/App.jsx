1090007414af967b248c8aa8dbdc0126ec29c659
maryam-tayep <mariemee233@gmail.com>
2026-03-30 13:18:40 +0200

    fix bootstrap imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Products from "./Pages/Products";
 import ProductDetail from "./Pages/ProductDetail";
 import Cart from "./Pages/Cart";
 import Navbar from "./components/NavBar";
-import "./App.css"
+import "./App.css";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
@@ -14,17 +14,16 @@ function App() {
   return (
     <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products/>} />
+        <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetail />} />
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
